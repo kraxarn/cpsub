bd91e8cf1ca9ba04d1e07481aceb9254a2fbfb65
kraxarn <me@kraxarn.com>
2020-03-29 13:22:16 +0200

    Add some comments

--- main.go
+++ main.go
@@ -8,26 +8,33 @@ import (
 	"strings"
 )
 
+// Copies file from source to target
 func copyFile(from, to string) error {
+	// Read source
 	inFile, err := ioutil.ReadFile(from)
 	if err != nil {
 		return err
 	}
+	// Write target
 	return ioutil.WriteFile(to, inFile, 0644)
 }
 
 func main() {
+	// Expect (at least) 4 arguments
 	if len(os.Args) < 4 {
 		fmt.Println("usage: cpsub <source> <target> <extension>")
 		return
 	}
+	// Recursively walk source directory
 	err := filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
+		// Something went wrong
 		if err != nil {
 			return err
 		}
 		// Full path of destination file
 		dest := strings.ReplaceAll(path, os.Args[1], "")
 		fullDest := filepath.Join(os.Args[2], dest)
+		// Create directory in target directory
 		if info.IsDir() {
 			if err = os.MkdirAll(fullDest, 0755);  err != nil {
 				return err
@@ -44,6 +51,7 @@ func main() {
 		}
 		return nil
 	})
+	// Print if something went wrong
 	if err != nil {
 		fmt.Println(err)
 	}
